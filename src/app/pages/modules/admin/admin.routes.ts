35a22852fb6deae8d509c35fa9051fd5f753027e
Sebastian <shuertas981@gmail.com>
2024-10-06 03:06:10 -0500

    Fondo de sidebar y titulo de sidebar

--- src/app/pages/modules/admin/admin.routes.ts
+++ src/app/pages/modules/admin/admin.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { DashboardComponent } from './modules/dashboard/dashboard.component';
 import { SalesComponent } from './modules/sales/sales.component';
+import { AdminComponent } from './admin.component';
+import { MoviesComponent } from './modules/movies/movies.component';
 
 export const adminRoutes: Routes = [
-    {path: '', component: DashboardComponent},
-    {path: 'ventas', component: SalesComponent}
+    {path: '', component: AdminComponent,
+        children: [
+            { path: '', redirectTo: 'dashboard', pathMatch: 'full' }, 
+            {path: 'dashboard', component: DashboardComponent},
+            {path: 'ventas', component: SalesComponent},
+            {path: 'peliculas', component: MoviesComponent}
+        ]
+    },
+  
 ];
