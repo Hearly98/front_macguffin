4aa03bde4095f72b9c8fcfca62a17f43ef008e23
Sebastian <shuertas981@gmail.com>
2024-10-08 08:11:13 -0500

    Componente navbar

--- src/app/pages/home/home-page/home-page.component.ts
+++ src/app/pages/home/home-page/home-page.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { RouterLinkActive } from '@angular/router';
+import { NavbarComponent } from "../components/navbar/navbar.component";
+import { FooterComponent } from '../components/footer/footer.component';
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, CommonModule],
-  templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.css'
+  imports: [RouterLink, RouterLinkActive, CommonModule, NavbarComponent, FooterComponent],
+  templateUrl: './home-page.component.html'
 })
 export class HomePageComponent {
 
